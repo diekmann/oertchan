1d0886582b6f87a1556ca7ded3b246766db8bfc8
diekmann <none@localhost>
2022-02-25 21:29:51 +0100

    ...

--- jslib/text_test.js
+++ jslib/text_test.js
@@ -12,16 +12,16 @@ describe('text', function() {
         it('empty', function() {
             assert.equal(parseMarkdown(buildLink, "").innerHTML, '');
         });
-        
+
         it('only text', function() {
             assert.equal(parseMarkdown(buildLink, "wololo").innerHTML, 'wololo');
         });
-        
+
         it('one link', function() {
             assert.equal(parseMarkdown(buildLink, "HeLLo, [some txt](http://href)!").innerHTML,
                 'HeLLo, <a href="http://href">some txt</a>!');
         });
-        
+
         it('two links', function() {
             assert.equal(parseMarkdown(buildLink, "HeLLo, [txt](href) yolo [txt2](href2)").innerHTML,
                 'HeLLo, <a href="href">txt</a> yolo <a href="href2">txt2</a>');
