0e92d629738034f7fb5a1b076d7558536ed3d08c
diekmann <none@localhost>
2022-02-26 14:52:50 +0100

    reac

--- main.js
+++ main.js
@@ -21,57 +21,93 @@ function logTxt_generic(txt) {
 logTxt_generic(`My uid: ${chans.uid}`)
 
 
-
-const chatBox = document.getElementById('chatBox');
 //const peerBox = document.getElementById('peerbox'); // defined in index.html
 
-function appendChatBox(elem) {
-    let t;
-    if (elem instanceof HTMLElement) {
-        t = elem
-    } else {
-        t = document.createElement("span");
-        t.appendChild(document.createTextNode(elem));
-    }
-    chatBox.appendChild(t);
-    chatBox.appendChild(document.createElement("br"));
-    t.scrollIntoView({
-        behavior: "smooth",
-        block: "end",
-        inline: "nearest"
-    });
-}
 
+// The chatBox handles broadcasted gosspied messages and is a global chat.
+const chatBox = (() => {
+    const elem = document.getElementById('chatBox');
 
-function formatChatBoxLink(chan) {
-    return (linkName, href) => {
-        const öhref = "ö" + (new URL(href, `rtchan://${chans.peerName(chan)}/`)).href;
-        let a = document.createElement('a');
-        a.innerText = linkName;
-        a.href = href;
-        a.onclick = (event) => {
-            event.preventDefault();
-            console.log(`link clicked for ${öhref}`, event);
-            peerBox.style.visibility = 'visible';
-            peerbox.getElementsByClassName("title")[0].getElementsByClassName("titletext")[0].innerText = `Connection to ${öhref}`;
-
-            chan.send(JSON.stringify({
-                request: {
-                    method: "GET",
-                    url: href,
-                },
-            }));
-            return false;
+    function append(content) {
+        let t;
+        if (content instanceof HTMLElement) {
+            t = content
+        } else {
+            t = document.createElement("span");
+            t.appendChild(document.createTextNode(content));
         }
-        return a;
+        elem.appendChild(t);
+        elem.appendChild(document.createElement("br"));
+        t.scrollIntoView({
+            behavior: "smooth",
+            block: "end",
+            inline: "nearest"
+        });
+    }
+
+
+    function formatLink(chan) {
+        return (linkName, href) => {
+            const öhref = "ö" + (new URL(href, `rtchan://${chans.peerName(chan)}/`)).href;
+            let a = document.createElement('a');
+            a.innerText = linkName;
+            a.href = href;
+            a.onclick = (event) => {
+                event.preventDefault();
+                console.log(`link clicked for ${öhref}`, event);
+                peerBox.style.visibility = 'visible';
+                peerbox.getElementsByClassName("title")[0].getElementsByClassName("titletext")[0].innerText = `Connection to ${öhref}`;
+
+                chan.send(JSON.stringify({
+                    request: {
+                        method: "GET",
+                        url: href,
+                    },
+                }));
+                return false;
+            }
+            return a;
+        };
     };
-};
+    
+    const sendMessageForm = document.getElementById('sendMessageForm');
+    const messageInputBox = document.getElementById('inputmessage');
+
+    // Handles clicks on the "Send" button by transmitting a message.
+    sendMessageForm.addEventListener('submit', function(event) {
+        console.log(`sendng message.`)
+
+        // don't actually submit the HTML form, stay on the same page.
+        event.preventDefault();
+
+        const message = messageInputBox.value;
+        for (let c of chans.chans) {
+            console.log("sending a message to", c);
+            c.send(JSON.stringify({
+                setPeerName: chans.uid,
+                message: message,
+            }));
+        }
+
+        append(formatMessage(chans.uid, parseMarkdown(formatLink(chans.loopbackChan), message)));
+
+        // Clear the input box and re-focus it, so that we're
+        // ready for the next message.
+        messageInputBox.value = "";
+        messageInputBox.focus();
+    }, false);
+    
+    return {
+        append: append,
+        formatLink: formatLink,
+    }
+})();
 
 
 const incomingMessageHandler = {
     message: (peerName, chan, message) => {
-        //appendChatBox(`From ${peerName(chan)}: ${d.message}`);
-        appendChatBox(formatMessage(peerName, parseMarkdown(formatChatBoxLink(chan), message)));
+        //chatBox.append(`From ${peerName(chan)}: ${d.message}`);
+        chatBox.append(formatMessage(peerName, parseMarkdown(chatBox.formatLink(chan), message)));
     },
     request: (peerName, chan, request) => {
         chan.send(JSON.stringify({
@@ -85,7 +121,7 @@ const incomingMessageHandler = {
         peerBoxContent.appendChild(document.createTextNode(JSON.stringify(response)));
     },
     default: (peerName, chan, data) => {
-        appendChatBox(formatMessage(peerName, document.createTextNode(`unknown contents: ${JSON.stringify(d)}`)));
+        chatBox.append(formatMessage(peerName, document.createTextNode(`unknown contents: ${JSON.stringify(d)}`)));
     },
 };
 
@@ -102,32 +138,3 @@ const onChanReady = (chan) => {
 chans.offerLoop((txt) => logTo(document.getElementById("logarea_offer"), txt), onChanReady, incomingMessageHandler);
 chans.acceptLoop((txt) => logTo(document.getElementById("logarea_accept"), txt), onChanReady, incomingMessageHandler);
 
-
-
-
-const sendMessageForm = document.getElementById('sendMessageForm');
-const messageInputBox = document.getElementById('inputmessage');
-
-// Handles clicks on the "Send" button by transmitting a message.
-sendMessageForm.addEventListener('submit', function(event) {
-    console.log(`sendng message.`)
-
-    // don't actually submit the HTML form, stay on the same page.
-    event.preventDefault();
-
-    const message = messageInputBox.value;
-    for (let c of chans.chans) {
-        console.log("sending a message to", c);
-        c.send(JSON.stringify({
-            setPeerName: chans.uid,
-            message: message,
-        }));
-    }
-
-    appendChatBox(formatMessage(chans.uid, parseMarkdown(formatChatBoxLink(chans.loopbackChan), message)));
-
-    // Clear the input box and re-focus it, so that we're
-    // ready for the next message.
-    messageInputBox.value = "";
-    messageInputBox.focus();
-}, false);
