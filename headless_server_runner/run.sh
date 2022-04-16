9a7b59497c6c03db5b931704f840594cb421b3c9
diekmann <nobody>
2022-04-16 20:30:23 +0200

    try hardening
    ich war mal containern

--- headless_server_runner/run.sh
+++ headless_server_runner/run.sh
@@ -7,4 +7,17 @@ wget --no-clobber --output-document=firefox.tar.bz2 'https://download.mozilla.or
 
 # TODO: cleanup if new ff downloaded
 tar -xjf firefox.tar.bz2
-./firefox/firefox --headless 'https://diekmann.github.io/oertchan/servers/wiki.html' 'https://diekmann.github.io/oertchan/servers/ping.html' 'https://diekmann.github.io/oertchan/servers/anonrelay.html'
+bwrap --new-session --ro-bind /usr /usr --dir /tmp --dir /var \
+	--ro-bind /etc/resolv.conf /etc/resolv.conf \
+	--proc /proc \
+	--dev /dev \
+	--symlink usr/lib /lib \
+	--symlink usr/lib64 /lib64 \
+	--symlink usr/bin /bin \
+	--unshare-all \
+	--share-net \
+	--die-with-parent \
+	--chdir / \
+	--ro-bind ./firefox /firefox \
+	--setenv PATH "/firefox:/usr/bin:/bin" \
+	/firefox/firefox --headless --new-instance 'https://diekmann.github.io/oertchan/servers/wiki.html' 'https://diekmann.github.io/oertchan/servers/ping.html' 'https://diekmann.github.io/oertchan/servers/anonrelay.html'
